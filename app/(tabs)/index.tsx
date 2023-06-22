8cc0f1f43830f10fb35254a827f51e5d337f49a9
Pablo <63468752+SuperPablo987@users.noreply.github.com>
2023-06-22 14:32:51 -0600

    new tweet page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
-import { StyleSheet, View, FlatList } from 'react-native';
+import { Entypo } from '@expo/vector-icons';
+import { StyleSheet, View, FlatList, Pressable } from 'react-native';
+import { Link } from 'expo-router';
 import tweets from '../../assets//data/tweets';
 import Tweet from '../../components/Tweet';
 
@@ -9,6 +11,13 @@ export default function TabOneScreen() {
         data={tweets} 
         renderItem={({item}) => <Tweet tweet={item} />}
       />
+        <Link href='/new-tweet' asChild>
+          <Entypo 
+            name="plus" 
+            size={24} 
+            color="white" 
+            style={styles.floatingButton}/>
+        </Link>
     </View>
   );
 }
@@ -17,5 +26,26 @@ const styles = StyleSheet.create({
   page:{
     flex:1,
     backgroundColor: 'white',
+  },
+  floatingButton: {
+    backgroundColor: '#1C9BF0',
+    width: 50,
+    height: 50,
+    borderRadius: 50,
+
+    textAlign: 'center',
+    lineHeight: 50,
+    position: 'absolute',
+    right: 15,
+    bottom: 15,
+    
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 7,
+    },
+    shadowOpacity:  0.21,
+    shadowRadius: 7.68,
+    elevation: 10
   },
 });
