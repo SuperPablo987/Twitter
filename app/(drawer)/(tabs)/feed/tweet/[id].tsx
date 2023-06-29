e45fa1f24b7ec2f40f1a40a25f51198c62a55810
Pablo <63468752+SuperPablo987@users.noreply.github.com>
2023-06-29 13:42:01 -0600

    fetch one tweet

--- app/(drawer)/(tabs)/feed/tweet/[id].tsx
+++ app/(drawer)/(tabs)/feed/tweet/[id].tsx
@@ -1,16 +1,24 @@
-import { Text } from 'react-native';
-import Tweet from '../../../../../components/Tweet';
-import tweets from '../../../../../assets/data/tweets';
+import { ActivityIndicator, Text } from 'react-native';
 import { useSearchParams } from 'expo-router';
+import { useQuery } from '@tanstack/react-query';
+
+import Tweet from '../../../../../components/Tweet';
+import { getTweet } from '../../../../../lib/api/tweets'
 
 export default function TweetScreen() {
     const {id} = useSearchParams();
 
-    const tweet = tweets.find(t => t.id === id);
+    const {data, isLoading, error} = useQuery({
+        queryKey: ['tweet', id],
+        queryFn:() => getTweet(id as string),
+    });
 
-    if(!tweet) {
-        return <Text>Tweet {id} not found!</Text>
+    if (isLoading) {
+        return <ActivityIndicator />;
     }
-    
-    return <Tweet tweet={tweet} />;
+    if (error) {
+        return <Text>Tweet {id} is not found!</Text>;
+    }
+
+    return <Tweet tweet={data} />;
 }
