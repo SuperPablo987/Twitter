97214038f4ad8035a1f7b46b4a357e5950c42833
Pablo <63468752+SuperPablo987@users.noreply.github.com>
2023-07-12 17:25:58 -0600

    connect to production aws instance

--- lib/api/config.ts
+++ lib/api/config.ts
@@ -1,2 +1,3 @@
-export const API_URL = "http://192.168.68.110:3000"; //okos host machine
+// export const API_URL = "http://192.168.68.110:3000"; //okos host machine
 // export const API_URL = "http://localhost:3000"; // does not work for me on my mobile device
+export const API_URL = "http://ec2-18-191-26-69.us-east-2.compute.amazonaws.com:3000"; //AWS server machine
